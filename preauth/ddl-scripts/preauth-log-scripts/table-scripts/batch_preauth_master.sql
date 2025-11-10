3ddc3641f41396af4340645a54373afefe2d3cc4
rajkamalrk <91298082+rajkamalrk@users.noreply.github.com>
2025-11-10 17:40:59 +0530

    Remove redundant DROP TABLE statement

--- preauth/ddl-scripts/preauth-log-scripts/table-scripts/batch_preauth_master.sql
+++ preauth/ddl-scripts/preauth-log-scripts/table-scripts/batch_preauth_master.sql
@@ -6,7 +6,7 @@ CREATE TABLE  txm_preauth_logs.batch_preauth_master (
 	failed_batch_id int ,
 	provider VARCHAR(100) NOT NULL,
 	file_type	VARCHAR(100),
-	file_name	VARCHAR(100),
+	file_name VARCHAR(100),
 	file_path	VARCHAR(255),
 	data_pipeline_id	VARCHAR(255),
 	status	VARCHAR(50) NOT NULL,
@@ -17,3 +17,4 @@ CREATE TABLE  txm_preauth_logs.batch_preauth_master (
 	update_user	VARCHAR(50)  NOT NULL,
 	update_timestamp	DATETIME(6)  NOT NULL
 ) ;
+
